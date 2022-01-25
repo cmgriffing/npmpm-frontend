4c807983be358825f793826db37012c5117cfad1
Chris Griffing <cmgriffing@gmail.com>
2022-01-25 12:37:47 -0800

    fix: use proper baseURL for production

--- app/utils/axios.ts
+++ app/utils/axios.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
-const baseURL = "http://localhost:3333";
-// const baseURL = "https://yzs3zsa9n7.execute-api.us-west-2.amazonaws.com";
+// const baseURL = "http://localhost:3333";
+const baseURL = "https://yzs3zsa9n7.execute-api.us-west-2.amazonaws.com";
 
 export const axios = Axios.create({
   baseURL,
