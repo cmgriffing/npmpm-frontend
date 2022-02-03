db96c67a6ad1b8a0ed16f6af324e01c0163bed97
Chris Griffing <cmgriffing@gmail.com>
2022-02-03 15:23:11 -0800

    feat: add twitch auth and improve button styling

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { Link, useNavigate, LoaderFunction, useLoaderData } from "remix";
 import { setToken } from "~/utils/token";
 import styles from "../styles/login.css";
+import githubLogo from "../images/GitHub-Mark-Light-64px.png";
+import twitchLogo from "../images/TwitchGlitchWhite.png";
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
@@ -13,16 +15,24 @@ export async function loader() {
       GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
       GITHUB_CALLBACK_URL: process.env.GITHUB_CALLBACK_URL,
       BASE_URL: process.env.BASE_URL,
+      TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
+      TWITCH_CALLBACK_URL: process.env.TWITCH_CALLBACK_URL,
     },
   };
 }
 
 export default function Login() {
   const { ENV } = useLoaderData();
-  const { GITHUB_CLIENT_ID, GITHUB_CALLBACK_URL } = ENV;
+  const {
+    GITHUB_CLIENT_ID,
+    GITHUB_CALLBACK_URL,
+    TWITCH_CLIENT_ID,
+    TWITCH_CALLBACK_URL,
+  } = ENV;
 
   const GITHUB_URL = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${GITHUB_CALLBACK_URL}&scopes=user`;
   const TWITTER_URL = "";
+  const TWITCH_URL = `https://id.twitch.tv/oauth2/authorize?client_id=${TWITCH_CLIENT_ID}&redirect_uri=${TWITCH_CALLBACK_URL}&response_type=code&scopes=user:read:email`;
 
   useEffect(() => {
     setToken("");
@@ -35,7 +45,7 @@ export default function Login() {
 
         <section className="login-list list-wrapper col">
           <h2 className="underlined-subtle">Session Based</h2>
-          <a className="btn btn-primary btn-github d-block m-2" href="/advc">
+          <a className="btn btn-primary btn-microsoft d-block m-2" href="/advc">
             Microsoft Identity
           </a>
           <details className="text-start">
@@ -53,7 +63,13 @@ export default function Login() {
             className="btn btn-primary btn-github d-block m-2"
             href={GITHUB_URL}
           >
-            Github
+            <img className="btn-logo" src={githubLogo} /> Github
+          </a>
+          <a
+            className="btn btn-primary btn-twitch d-block m-2"
+            href={TWITCH_URL}
+          >
+            <img className="btn-logo" src={twitchLogo} /> Twitch
           </a>
           {/*
       <a className="btn btn-primary btn-github d-block m-2" href={TWITTER_URL}>
